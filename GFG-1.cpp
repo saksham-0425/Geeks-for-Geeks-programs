6cd872df15d4b9a7034882ca84bf6e41ca891003
Saksham Gupta <124126440+saksham-0425@users.noreply.github.com>
2023-08-16 14:03:07 +0530

    Add files via upload

---
+++ GFG-1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+        int n;
+        cin>>n;
+        int array[n];
+        for(int  i=0;i<n-1;i++){
+            cin>>array[i];
+        }
+        
+        int total=(n)*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+            sum+=array[i];
+        }
+        
+        cout<<total-sum;
+        
+        
+    
+    return 0;
+}
